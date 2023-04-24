95a525eccd1fdc3af8e44e9cc0dcac1042bf8336
Ryan <ryan.wildanitahmid17@gmail.com>
2023-04-25 01:54:30 +0700

    task bang bilal

--- script.js
+++ script.js
@@ -10,7 +10,31 @@ function display(value) {
 
 // hasilnya
 function calculate() {
-    var p = document.getElementById("hasil").value;
-    var q = eval(p);
-    document.getElementById("hasil").value = q;
+    let p = document.getElementById("hasil").value;
+    let q = eval(p);
+    if (q == undefined) {
+        document.getElementById("hasil").value = ""
+    } else {
+        document.getElementById("hasil").value = q;
+    }
 }
+
+// Jika dienter hasil yang ada di inputNama akan ditaro di Child table nama
+// Table namanya input tr yang isinya td inputNama
+const tableNama = document.querySelector('.table-nama');
+const enterNama = document.getElementById('enterNama');
+let inputNama = document.getElementById('inputNama');
+
+enterNama.addEventListener('click', function() {
+    if (inputNama.value == "" ) {
+        alert('masukkan sebuah nama dulu dong');
+    } else {
+        let parrentTrBaru = document.createElement('tr');
+        let tdBaru = document.createElement('td');
+        let teksTdBaru = document.createTextNode(inputNama.value);
+        
+        tdBaru.appendChild(teksTdBaru);
+        parrentTrBaru.appendChild(tdBaru);
+        tableNama.appendChild(parrentTrBaru);
+    };
+});
