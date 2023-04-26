4b15d92c7f57a10c20c8ebcf75cfc09152d364be
Ryan <ryan.wildanitahmid17@gmail.com>
2023-04-26 19:44:54 +0700

    blm selesai

--- script.js
+++ script.js
@@ -25,16 +25,42 @@ const tableNama = document.querySelector('.table-nama');
 const enterNama = document.getElementById('enterNama');
 let inputNama = document.getElementById('inputNama');
 
+
+
+
 enterNama.addEventListener('click', function() {
     if (inputNama.value == "" ) {
         alert('masukkan sebuah nama dulu dong');
     } else {
+        let sp = document.createElement('span');
+        let teksX = document.createTextNode('x');
+        sp.appendChild(teksX); 
+        
+
         let parrentTrBaru = document.createElement('tr');
         let tdBaru = document.createElement('td');
         let teksTdBaru = document.createTextNode(inputNama.value);
         
         tdBaru.appendChild(teksTdBaru);
+        tdBaru.appendChild(sp);
         parrentTrBaru.appendChild(tdBaru);
         tableNama.appendChild(parrentTrBaru);
+        sp.classList.add('silang');
     };
 });
+
+
+//Jika x span diclick maka parent tr nya hilang
+const silang = document.querySelectorAll('.silang');
+
+/* for(let i = 0; i < silang.length; i++) {
+    silang[i].addEventListener('click', function (e) {
+        e.target.parentElement.parentElement.style.display = 'none'
+    });
+}; */
+
+silang.forEach(function(tiapSilang) {
+    tiapSilang.addEventListener('click', function(e) {
+        e.target.parentElement.parentElement.style.display = 'none'
+    })
+})
